481476ebc34503ec662f1067e25424740308f969
PrasannaPandugula <prasannapandugula@gmail.com>
2026-02-01 17:33:45 +0530

    updated terraform script

--- EKS-TF/iam-eks-policy.tf
+++ EKS-TF/iam-eks-policy.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_policy_attachment" "AmazonEKSClusterPolicy " {
+resource "aws_iam_policy_attachment" "AmazonEKSClusterPolicy" {
   name       = "AmazonEKSClusterPolicy"
   roles      = [aws_iam_role.EKSClusterRole.name]
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
@@ -10,7 +10,7 @@ resource "aws_iam_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_policy_attachment" "AmazonEC2ContainerRegistryReadOnly " {
+resource "aws_iam_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   name       = "AmazonEC2ContainerRegistryReadOnly"
   roles      = [aws_iam_role.NodeGroupRole]
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
